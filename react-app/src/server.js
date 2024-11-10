995dc5a477104e34310a8f5ab833f02e5f5c2751
Lukas <lukas.jehle@gmail.com>
2024-11-10 20:35:30 +1300

    update react-app launch configs

--- react-app/src/server.js
+++ react-app/src/server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 const port = 3002;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-const dbHost = process.env.DB_HOST;
-const dbName = process.env.DB_NAME;
-const dbPort = process.env.DB_PORT;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+
+if (![DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT].every(Boolean)) {
+  console.error('ERROR: Database environment variables are not all set.');
+  process.exit(1);
+}
+
+const dbUser = DB_USER;
+const dbPassword = DB_PASSWORD;
+const dbHost = DB_HOST;
+const dbName = DB_NAME;
+const dbPort = DB_PORT;
 
 const connection = mysql.createConnection({
   host: dbHost,
